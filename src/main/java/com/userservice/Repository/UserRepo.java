da8f51c657e49e7f9698c7ac770a85ef0b41432f
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 16:08:14 +0530

    Boiler Plate created for User service

---
+++ src/main/java/com/userservice/Repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.userservice.Repository;
+
+import com.userservice.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepo extends MongoRepository<User,String> {
+
+}
---
+++ src/main/java/com/userservice/Service/UserService.java
@@ -0,0 +1,19 @@
+package com.userservice.Service;
+
+
+import com.userservice.Model.User;
+import com.userservice.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+
+}
