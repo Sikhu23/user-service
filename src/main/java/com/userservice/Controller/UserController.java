5f772b53cd77862b81fd24cebb4214feaeb30adc
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-22 10:51:52 +0530

    removed some bugs

--- src/main/java/com/userservice/Controller/UserController.java
+++ src/main/java/com/userservice/Controller/UserController.java
@@ -6,7 +6,9 @@ import com.userservice.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +18,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+   
 
 
 
