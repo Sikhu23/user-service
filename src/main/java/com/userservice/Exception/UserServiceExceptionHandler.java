0f0ce8be0559edbaca72d390fb1d34857ea9ef85
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-23 15:58:24 +0530

    Added Variable Exception Handling

--- src/main/java/com/userservice/Exception/UserServiceExceptionHandler.java
+++ src/main/java/com/userservice/Exception/UserServiceExceptionHandler.java
@@ -3,14 +3,20 @@ package com.userservice.Exception;
 
 import com.userservice.Model.ApiError;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @ControllerAdvice
@@ -24,33 +30,24 @@ class UserServiceExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
 
-//
-//    @ExceptionHandler({CustomerAlreadyExistsException.class})
-//    ResponseEntity customerAlreadyExistsExceptionHandler(Exception exception, ServletWebRequest request){
-//        ApiError apiError = new ApiError();
-//        apiError.setStatus(HttpStatus.NOT_FOUND);
-//        apiError.setErrors(Arrays.asList(exception.getMessage()));
-//        apiError.setPath(request.getDescription(false));
-//        apiError.setTimestamp(LocalDateTime.now());
-//        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
-//    }
 
-//    @Override
-//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-//                                                                  HttpHeaders headers, HttpStatus status,
-//                                                                  WebRequest request) {
-//
-//        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-//
-//        List<String> errors = fieldErrors.stream()
-//                .map(err -> err.getField() + " : " + err.getDefaultMessage())
-//                .collect(Collectors.toList());
-//
-//
-//        ApiError apiError = new ApiError();
-//        apiError.setCode();
-//        apiError.setMessage();
-//
-//        return new ResponseEntity<>(apiError, headers, apiError.getCode());
-//    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        List<String> errors = fieldErrors.stream()
+                .map(err -> err.getField() + " : " + err.getDefaultMessage())
+                .collect(Collectors.toList());
+
+
+        ApiError apiError = new ApiError();
+        apiError.setCode("400");
+        apiError.setMessage(String.valueOf(errors));
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
 }
